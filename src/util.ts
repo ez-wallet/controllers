1431ca934c0d5f636a74ef896648b67eacd6f3a3
Frederik Bolding <frederik.bolding@gmail.com>
2022-06-03 11:45:29 +0200

    Basic Ethers implementation for AssetsContractController

--- src/util.ts
+++ src/util.ts
@@ -26,6 +26,7 @@ import { TypedMessageParams } from './message-manager/TypedMessageManager';
 import { Token } from './assets/TokenRatesController';
 import { MAINNET } from './constants';
 import { Json } from './BaseControllerV2';
+import { StaticWeb3Provider } from './StaticWeb3Provider';
 
 const TIMEOUT_ERROR = new Error('timeout');
 
@@ -1022,4 +1023,8 @@ function logOrRethrowError(error: any, codesToCatch: number[] = []) {
   } else {
     throw error;
   }
+}
+
+export function getEthersProvider(provider: any, chainId?: number) {
+  return new StaticWeb3Provider(provider, chainId);
 }
